f1dcd436f94d21ea3601285efdc01c48ce55de3d
mark lang <langjie@adsage.com>
2015-05-26 11:38:08 +0800

    add missing class

---
+++ testssh/src/cn/itcast/oa/service/PrivilegeService.java
@@ -0,0 +1,10 @@
+package cn.itcast.oa.service;
+
+import org.springframework.stereotype.Service;
+
+import cn.itcast.oa.base.BaseDao;
+import cn.itcast.oa.domain.Privilege;
+
+public interface PrivilegeService extends BaseDao<Privilege> {
+
+}
---
+++ testssh/src/cn/itcast/oa/service/impl/PrivilegeServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.itcast.oa.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import cn.itcast.oa.base.BaseDaoImpl;
+import cn.itcast.oa.domain.Privilege;
+import cn.itcast.oa.service.PrivilegeService;
+@Service
+public class PrivilegeServiceImpl extends BaseDaoImpl<Privilege>implements PrivilegeService {
+
+}
