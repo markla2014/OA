6fc1df01e6a29e60134a0345cdeca75ec90d6348
mark lang <langjie@adsage.com>
2015-05-26 22:40:19 +0800

    same as distination

---
+++ testssh/src/cn/itcast/oa/service/impl/PrivilegeServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.itcast.oa.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import cn.itcast.oa.base.BaseDaoImpl;
+import cn.itcast.oa.domain.Privilege;
+import cn.itcast.oa.service.PrivilegeService;
+@Service
+public class PrivilegeServiceImpl extends BaseDaoImpl<Privilege>implements PrivilegeService {
+
+	@Override
+	public List<Privilege> findTopList() {
+		
+		return getSession().createQuery("from Privilege p where p.parent is NuLL").list();
+	}
+
+}
