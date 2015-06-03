6317f0157756bbb559a10b702a9c5b7219db8366
mark lang <langjie@adsage.com>
2015-06-03 21:01:28 +0800

    3/6 cmplete

--- testssh/src/cn/itcast/oa/domain/Forum.java
+++ testssh/src/cn/itcast/oa/domain/Forum.java
@@ -1,5 +1,8 @@
 package cn.itcast.oa.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /*板块
  *  
  */
@@ -8,6 +11,36 @@ public class Forum {
  private String name;
  private String description;
  private int position;
+ 
+ private Set<Topic> topics=new HashSet<Topic>();
+ private int topicCount;
+ private int articleCount;
+ private Topic lastTopic;
+ 
+public Set<Topic> getTopics() {
+	return topics;
+}
+public void setTopics(Set<Topic> topics) {
+	this.topics = topics;
+}
+public int getTopicCount() {
+	return topicCount;
+}
+public void setTopicCount(int topicCount) {
+	this.topicCount = topicCount;
+}
+public int getArticleCount() {
+	return articleCount;
+}
+public void setArticleCount(int articleCount) {
+	this.articleCount = articleCount;
+}
+public Topic getLastTopic() {
+	return lastTopic;
+}
+public void setLastTopic(Topic lastTopic) {
+	this.lastTopic = lastTopic;
+}
 public Long getId() {
 	return id;
 }
